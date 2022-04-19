35dba0d83ab299fddc203864b3c940eb6ccdac32
FrodoDeTero <102458774+FrodoDeTero@users.noreply.github.com>
2022-04-19 16:45:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 
+class Cell{
+    constructor(ele){
+        this.ele = ele;
+        this.bgcolor = "#00000000";
+        this.content = ""
+    }
+}
+
+function newBoard(height,width){
+    let eBoard=document.createElement("table");
+        let l = [];
+        for(i=0;i<width;i++){
+            l.append([]);
+            for(j=0;j<height;j++){
+                l[i].append(new Cell);
+            }
+        }
+
+    }
+
