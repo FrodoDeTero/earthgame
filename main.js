e6c4de1fbce0d01abeffeba2098d7b0dd597b214
FrodoDeTero <102458774+FrodoDeTero@users.noreply.github.com>
2022-04-21 09:51:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,13 +9,17 @@ class Cell{
 
 function newBoard(height,width){
     let eBoard=document.createElement("table");
-        let l = [];
-        for(i=0;i<width;i++){
-            l.append([]);
-            for(j=0;j<height;j++){
-                l[i].append(new Cell);
-            }
+    let l = [];
+    for(i=0;i<width;i++){
+        l.append([]);
+        let el=document.createElement("tr")
+        eBoard.appendChild(el)
+        for(j=0;j<height;j++){
+            let ell=document.createElement("td")
+            el.appendChild(ell)
+            l[i].append(Cell(ell));
         }
-
     }
-
+    return l
+}
+var liste = newBoard(6,5)
